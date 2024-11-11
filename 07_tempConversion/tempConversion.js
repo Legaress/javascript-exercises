029507b9f5401c0f92a3e53eb142974ab177095f
Lesnier Garcia <lesnier.estrada@gmail.com>
2024-11-11 15:29:21 -0500

    feat: 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = (number) => Math.round(number * 10) / 10;
+
+const convertToCelsius = (temperatureFahrenheit) => {
+  
+  const temperatureCelsius = (temperatureFahrenheit - 32) * (5 / 9);
+  return roundToOneDecimal(temperatureCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (temperatureCelsius) => {
+
+  const temperatureFahrenheit = (temperatureCelsius * (9 / 5)) + 32;
+  return roundToOneDecimal(temperatureFahrenheit);
 };
+
 
 // Do not edit below this line
 module.exports = {
