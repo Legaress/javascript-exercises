0c0800db9b6658b44311fc0644cd76418d825fd9
Lesnier Garcia <lesnier.estrada@gmail.com>
2024-11-15 11:07:37 -0500

    feat: Solved 09_palindormes lesson

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string = '') {
 
-};
+    // Normalize the string: remove non-alphanumeric characters and convert to lowercase
+    const normalizedString = string.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    
+    let left = 0;
+    let right = normalizedString.length - 1;
+
+    while (left < right) {
+        if (normalizedString[left] !== normalizedString[right]) {
+            return false; // Early exit if mismatch found
+        }
+        left++;
+        right--;
+    }
+
+    return true; // If no mismatches were found
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
