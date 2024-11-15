564dcd3180ab51486d63bada21d550db1fe1813a
Lesnier Garcia <lesnier.estrada@gmail.com>
2024-11-15 15:25:44 -0500

    feat: Solved 12_findTheOldest lesson

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    const oldestPerson = people.reduce((oldest, currentPerson) => {
+        // Determine the year of death for the current person
+        const yearOfDeath = currentPerson.yearOfDeath || currentYear;
+        
+        // Calculate age
+        const age = yearOfDeath - currentPerson.yearOfBirth;
+
+        // Compare ages and return the oldest
+        return (age > (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth) ? currentPerson : oldest;
+    });
+
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
