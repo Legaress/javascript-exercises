ce11763e201905c3ac8eb68f4b39e6409e99fd3b
Lesnier Garcia <lesnier.estrada@gmail.com>
2024-11-15 11:46:03 -0500

    feat: Solved 10_fibonacci lesson

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const PHI = (1 + Math.sqrt(5)) / 2;
+const PSI = (1 - Math.sqrt(5)) / 2; // Conjugate of PHI
 
+const fibonacci = function(n) {
+    // Convert string input to number
+    if (typeof n === 'string') {
+        n = parseInt(n, 10); // Convert string to integer
+    }
+
+    // Input validation
+    if (typeof n !== 'number' || n < 0 || !Number.isInteger(n)) {
+        return "OOPS";
+    }
+
+    // Binet's formula to calculate the nth Fibonacci number
+    const fibN = (Math.pow(PHI, n) - Math.pow(PSI, n)) / Math.sqrt(5);
+    
+    return Math.round(fibN);
 };
 
 // Do not edit below this line
